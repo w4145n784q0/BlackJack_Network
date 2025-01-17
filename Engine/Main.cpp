356ddeac47c0227981885efa2a853afcd147e05d
Moe <230090@jc-21.jp>
2025-01-17 16:30:00 +0900

    接続テストしてるところ クライアントにはおくれてるぽい サーバーには来ない 一連の文をどこに置くか(whileループかなあ)がわからない

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -17,6 +17,12 @@ namespace Screen {
 // エントリーポイント
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	char StrBuf[256];        // データバッファ
+	int NetHandle, LostHandle;    // ネットワークハンドル
+	int DataLength;            // 受信データ量保存用変数
+	IPDATA Ip;            // 接続先ＩＰアドレスデータ
+
+
 	SetGraphMode(Screen::WIDTH, Screen::HEIGHT, 32);
 	SetOutApplicationLogValidFlag(FALSE); // ログを出さない
 
@@ -36,11 +42,65 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	RootObject* pRootObject = new RootObject;
 	pRootObject->Initialize();
 
+	// 接続してくるのを待つ状態にする
+	PreparationListenNetWork(9850);
+
+	// 接続してくるかＥＳＣキーが押されるまでループ
+	NetHandle = -1;
+	while (!ProcessMessage() && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		// 新しい接続があったらそのネットワークハンドルを得る
+		NetHandle = GetNewAcceptNetWork();
+		if (NetHandle != -1) break;
+	}
+
+	// 接続されていたら次に進む
+	if (NetHandle != -1)
+	{
+		// 接続の受付を終了する
+		StopListenNetWork();
+
+		// 接続してきたマシンのＩＰアドレスを得る
+		GetNetWorkIP(NetHandle, &Ip);
+
+		// データが送られて来るまで待つ
+		while (!ProcessMessage())
+		{
+			// 取得していない受信データ量が０以外のときはループから抜ける
+			if (GetNetWorkDataLength(NetHandle) != 0) break;
+		}
+
+		// データ受信
+		DataLength = GetNetWorkDataLength(NetHandle);    // データの量を取得
+		NetWorkRecv(NetHandle, StrBuf, DataLength);    // データをバッファに取得
+
+		// 受信したデータを描画
+		DrawString(0, 0, StrBuf, GetColor(255, 0, 0));
+
+		// 受信成功のデータを送信
+		NetWorkSend(NetHandle, "tunagattayo", 17);
+
+		// 相手が通信を切断するまで待つ
+		while (!ProcessMessage())
+		{
+			// 新たに切断されたネットワークハンドルを得る
+			LostHandle = GetLostNetWork();
+
+			// 切断された接続が今まで通信してた相手だった場合ループを抜ける
+			if (LostHandle == NetHandle) break;
+		}
+
+		// 切断確認表示
+		DrawString(0, 16, "setudan", GetColor(255, 255, 255));
+
+		// キー入力待ち
+		WaitKey();
+	}
+
 	while (true) {
 	//全オブジェクトの更新処理
 	//ルートオブジェクトのUpdateを呼んだあと、自動的に子、孫のUpdateが呼ばれる
 		pRootObject->UpdateSub();
-
 
 		if (DxLib::ProcessMessage() == -1 /*|| AppIsExit()*/)
 			break;
@@ -49,7 +109,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		//全オブジェクトを描画
 		//ルートオブジェクトのDrawを呼んだあと、自動的に子、孫のUpdateが呼ばれる
 		pRootObject->DrawSub();
-
 
 		RefreshDxLibDirect3DSetting();
 
