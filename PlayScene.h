6e88c5e88b55ee512c00d1f2ca904c0a6dcba87a
mota <2910m@>
2025-02-23 15:09:46 +0900

    でぎだ！(願望)

--- PlayScene.h
+++ PlayScene.h
@@ -27,15 +27,21 @@ namespace
 class PlayScene :
 	public GameObject
 {
+	//プレイヤー情報
 	vector<PLAYER> playerInfos; //各プレイヤーの情報 0:自分 1-3: クライアント(クライアント数が変わるかもなので可変)
 	PLAYER myInfo; //サーバー側 自分の情報
-	int trump[TRUMP_NUM]; //トランプの配列(各4ずつ)->選ばれたら引いていく感じ
-	vector<int> myCards; //自分のカードの配列
-	int hitCard; //ヒットしたカードの番号
-	bool isChosenHit; //ヒットが選ばれたかどうか
-	map<int, vector<int>> playerCards; //プレイヤーのカードの配列 key:ID value:カードの数字
-	//ヒットしたカードを保存しておくなにかが必要
 
+	//トランプ情報
+	int trump[TRUMP_NUM]; //トランプの配列(各4ずつ)->選ばれたら引いていく感じ
+	
+	//引いたカード情報
+	vector<int> myCards; //自分のカードの配列
+	map<int, vector<int>> playerCards; //プレイヤーのカードの配列 key:ID value:カードの数字
+	map<int, int> playerPoints; //プレイヤーの点数の配列 key:ID value:点数 0-2がクライアント(id対応) 3がディーラー
+
+	//その他
+	bool isChosenHit; //ヒットが選ばれたかどうか
+	int winnerId;
 public:
 	PlayScene();
 	~PlayScene();
@@ -45,13 +51,13 @@ public:
 	void Draw() override;
 	void Release() override;
 	void GiveCards(); //カード配る
-	int Choose(); //ヒットかスタンドか選ぶ 全員がスタンドになるまでやる！！
+	int Choose(int clientId); //ヒットとスタンドの処理 全員がスタンドになるまでやる！！
 	void Judge(); //勝敗つける、ポイント
-
 
 	//アクセス関数
 	map<int, vector<int>> GetCards() { return playerCards; }
 	void ChooseHit(bool hit) { isChosenHit = hit; }
+	map<int, int> GetPoint() { return playerPoints; }
 };
 
 
